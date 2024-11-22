9fda854fc1dc1875f3b081bfe60bc7643c8b10fd
Arkeshke <163832451+Arkeshke@users.noreply.github.com>
2024-11-22 23:00:34 +0300

    Add files via upload

---
+++ styles/reg.js
@@ -0,0 +1,11 @@
+document.getElementById('myButton').onclick = myFunction;
+
+function myFunction() {
+  alert('you are sing up!');
+}
+
+document.getElementById('myButton').onclick = sea;
+
+function sea() {
+    alert('you are sing in!');
+  }
